95bd4f23f5b13ff321091b2a574f439e901eb208
malo21st <44739330+malo21st@users.noreply.github.com>
2021-03-27 08:53:16 +0900

    Update report.py

--- report.py
+++ report.py
@@ -48,21 +48,21 @@ with col2:
     
 # 検索
 msg, data = get_report(name, key_word)
-
+data
 # 検索結果（表）
-HEADER = '| 管理No. | 　報　告　書　名 | 委託先 | 報告書 | デ｜タ |\n|:-:|:--|:-:|:-:|:-:|\n'
-if isinstance(data, int):
-    st.markdown(msg)
-elif isinstance(data, pd.core.frame.DataFrame):
-    result = HEADER
-    df_report = data.tail(20)
-    for i, r in df_report[::-1].iterrows():
-        if r[8] == "":
-            row = "|{}|{}|{}|[●]({})||\n".format(str(r[2]).zfill(6), r[3], r[4], r[7])
-        else:
-            row = "|{}|{}|{}|[●]({})|[●]({})|\n".format(str(r[2]).zfill(6), r[3], r[4], r[7], r[8])
-        result += row
-    st.markdown(result)
+# HEADER = '| 管理No. | 　報　告　書　名 | 委託先 | 報告書 | デ｜タ |\n|:-:|:--|:-:|:-:|:-:|\n'
+# if isinstance(data, int):
+#     st.markdown(msg)
+# elif isinstance(data, pd.core.frame.DataFrame):
+#     result = HEADER
+#     df_report = data.tail(20)
+#     for i, r in df_report[::-1].iterrows():
+#         if r[8] == "":
+#             row = "|{}|{}|{}|[●]({})||\n".format(str(r[2]).zfill(6), r[3], r[4], r[7])
+#         else:
+#             row = "|{}|{}|{}|[●]({})|[●]({})|\n".format(str(r[2]).zfill(6), r[3], r[4], r[7], r[8])
+#         result += row
+#     st.markdown(result)
     
 # 出典
 st.markdown("出典：[委託調査報告書（METI/経済産業省）](https://www.meti.go.jp/topic/data/e90622aj.html)")    
