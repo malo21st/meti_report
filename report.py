97680ccad0ac6656e61d356f863369dc20867df5
malo21st <44739330+malo21st@users.noreply.github.com>
2021-03-27 02:43:56 +0900

    Update report.py

--- report.py
+++ report.py
@@ -39,7 +39,7 @@ def get_report(name, key_word):
 
 st.title("委託調査報告書(経済産業省)検索サービス")
 
-col1, col2 = st.beta_columns((1,4))
+col1, col2 = st.beta_columns((1,5))
 with col1:
     name = st.radio("項　目：", ("報告書名", "委託先"))
 with col2:
