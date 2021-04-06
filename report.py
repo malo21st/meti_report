45e881cfe894fcf097525e0c7b643571f9539e3e
malo21st <44739330+malo21st@users.noreply.github.com>
2021-04-07 07:45:58 +0900

    Update report.py

--- report.py
+++ report.py
@@ -81,6 +81,7 @@ if isinstance(data, pd.core.frame.DataFrame): # インスタンスdata が DataF
             row += "|\n"    
         result += row
     st.markdown(result)
+    st.markdown("●：リンクあり，×：リンクなし")
     
 #【表示】出典
 st.markdown("出典：[委託調査報告書（METI/経済産業省）](https://www.meti.go.jp/topic/data/e90622aj.html)")    
