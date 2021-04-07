82a236133d7adfaa73bb337b9f51cef9b6ed6698
malo21st <44739330+malo21st@users.noreply.github.com>
2021-04-07 10:02:57 +0900

    Update report.py

--- report.py
+++ report.py
@@ -15,9 +15,6 @@ def get_connection():
 conn = get_connection()
 
 def get_sql(name: str, key_word: str) -> pd.core.frame.DataFrame:
-""" 関数の概要
-    RDBをSQLで検索する
-"""
     if name == "報告書名":
         lst_kw = ["report LIKE '%{}%'".format(kw) for kw in  key_word.split()]
     elif name == "委託先":
@@ -27,9 +24,6 @@ def get_sql(name: str, key_word: str) -> pd.core.frame.DataFrame:
     return df_sql
 
 def get_report(name: str, key_word: str) -> str, pd.core.frame.DataFrame:
-""" 関数の概要
-    項目名とキーワードで検索し、メッセージと検索結果を返す
-"""
     if key_word == "":
         msg, data = "項目を選択して、キーワードを入力して下さい。", 0
     elif "%" in key_word:
