b84e79d686dbba36049cfa716bd1cc566f178bcf
malo21st <44739330+malo21st@users.noreply.github.com>
2021-05-01 22:48:22 +0900

    Update report.py

--- report.py
+++ report.py
@@ -94,5 +94,5 @@ with st.beta_expander("概要・使い方・出典"):
     st.markdown("- 使い方：キーワードを入力後、**「エンター」キーを押すと検索**します。")
     st.markdown("- 使い方：**空白区切りで、複数キーワード**をＡＮＤ条件で検索できます。")
     st.markdown("- その他：検索結果は、登録が**新しい報告書から最大50件**を表示してます。")
-    st.markdown("- 説　明：[お宝の委託調査報告書をもっと閲覧して欲しい(PyData.Fukuoka)](https://docs.google.com/presentation/d/1j13WnD8AgiiYprLIZkI1dbgNlryQDmWY8g5KRbHuiiM/edit?usp=sharing)")
+    st.markdown("- 資　料：[お宝の委託調査報告書をもっと閲覧して欲しい(PyData.Fukuoka)](https://docs.google.com/presentation/d/1j13WnD8AgiiYprLIZkI1dbgNlryQDmWY8g5KRbHuiiM/edit?usp=sharing)")
     st.markdown("- 出　典：[委託調査報告書（METI/経済産業省）](https://www.meti.go.jp/topic/data/e90622aj.html)")
